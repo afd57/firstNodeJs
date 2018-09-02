362b04c10e854268068816e20698373447f12245
Aziz <afdagli@gmail.com>
2018-09-02 21:44:17 +0300

    Tab change to 4 space

--- server_calisma.js
+++ server_calisma.js
@@ -5,63 +5,63 @@ var path = require('path');
 
 function login(request,response)
 {
-	if(request.url == '/')
-	{
-		console.log(request.url);
-		response.writeHeader(200,{'Context-Type':'text/html'});
-		var index_ = fs.readFileSync("./index.html");
-		response.write(index_)
-		response.end();
+    if(request.url == '/')
+    {
+        console.log(request.url);
+        response.writeHeader(200,{'Context-Type':'text/html'});
+        var index_ = fs.readFileSync("./index.html");
+        response.write(index_)
+        response.end();
 
-	}
-	else
-	{
-		response.writeHeader(200,{'Context-Type':'jpeg'});
-		var content_ = fs.readFileSync(request.url);
-		response.write(process.cwd()+content_);
-		console.log(process.cwd()+request.url)
-		response.end();
-	}
-	
+    }
+    else
+    {
+        response.writeHeader(200,{'Context-Type':'jpeg'});
+        var content_ = fs.readFileSync(request.url);
+        response.write(process.cwd()+content_);
+        console.log(process.cwd()+request.url)
+        response.end();
+    }
+    
 }
 
 
 
 function login2(req,res)
 {
-	var filePath = req.url;
-	if (filePath == '/'){
-		filePath = '/index.html';
-	}
+    var filePath = req.url;
+    if (filePath == '/'){
+        filePath = '/index.html';
+    }
 
-	filePath = __dirname+filePath;
-	var extname = path.extname(filePath);
-	var contentType = 'text/html';
+    filePath = __dirname+filePath;
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
 
-	switch (extname) {
-		case '.js':
-			contentType = 'text/javascript';
-			break;
-		case '.css':
-			contentType = 'text/css';
-			break;
-	}
-	
-	fs.exists(filePath, function(exists) {
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+    
+    fs.exists(filePath, function(exists) {
 
     if (exists) {
-		fs.readFile(filePath, function(error, content) {
-			if (error) {
-				res.writeHead(500);
-				res.end();
-			}
-			else {
-				res.writeHead(200, { 'Content-Type': contentType });
-				res.end(content, 'utf-8');
-			}
-		});
-	}
-	});
+        fs.readFile(filePath, function(error, content) {
+            if (error) {
+                res.writeHead(500);
+                res.end();
+            }
+            else {
+                res.writeHead(200, { 'Content-Type': contentType });
+                res.end(content, 'utf-8');
+            }
+        });
+    }
+    });
 }
 
 http.createServer(login2).listen(8080);
